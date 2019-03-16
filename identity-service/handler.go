1b06b49eee17a70bc526e91a7cae841896fb6a91
dennisyd <dennisyd@alum.mit.edu>
2019-03-15 22:42:01 -0700

    Implemented auth method and fixed create to ensure email is unique

--- identity-service/handler.go
+++ identity-service/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	pb "github.com/dins-app/web-services/proto/identity-service"
@@ -15,6 +16,11 @@ type Handler struct{}
 func (h *Handler) Create(ctx context.Context, u *pb.User) (*pb.Response, error) {
 	log.Print("Received request")
 	res := pb.Response{}
+	if !DB.Where(map[string]string{"email": u.Email}).First(&u).RecordNotFound() {
+		u.Password = ""
+		res.Errors = append(res.Errors, fmt.Sprintf("account %s already exists", u.Email))
+		return &res, nil
+	}
 	password, err := bcrypt.GenerateFromPassword([]byte(u.Password), 10)
 	if err != nil {
 		res.Errors = append(res.Errors, err.Error())
@@ -49,6 +55,21 @@ func (h *Handler) Get(ctx context.Context, r *pb.Request) (*pb.Response, error) 
 
 // Auth authorizes a user by email and password
 func (h *Handler) Auth(ctx context.Context, u *pb.User) (*pb.Response, error) {
+	log.Print("Received request")
 	res := pb.Response{}
+	user := pb.User{}
+	errors := DB.Where(map[string]string{"email": u.Email}).First(&user).GetErrors()
+	for _, err := range errors {
+		res.Errors = append(res.Errors, err.Error())
+	}
+	if len(errors) == 0 {
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(u.Password))
+		if err != nil {
+			res.Errors = append(res.Errors, err.Error())
+			return &res, nil
+		}
+		user.Password = ""
+		res.User = &user
+	}
 	return &res, nil
 }
